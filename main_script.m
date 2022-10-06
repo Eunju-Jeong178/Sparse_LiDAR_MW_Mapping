8076333a60394dd2a2dcddbe154541ce5bad1b9c
Eunju Jeong <77608922+Eunju-Jeong178@users.noreply.github.com>
2022-10-06 21:02:07 +0900

    Update main_script.m

--- main_script.m
+++ main_script.m
@@ -23,10 +23,11 @@ delimiter = ' ';
 headerlinesIn = 1;
 milliSecondToSecond = 1000;
 
-%% Parse Crazyflie 6 DoF pose data by Optitrack
+%% 1.Parse Crazyflie 6 DoF pose data by Optitrack
 
 % parsing Crazyflie(CF) pose data text file 
-textFileDir_optitrack = 'input\FlowdeckTime_Optitrack_Crazyflie_6DoF_pose.txt'
+%textFileDir_optitrack = 'input\FlowdeckTime_Optitrack_Crazyflie_6DoF_pose.txt'
+textFileDir_optitrack = 'input\Crazyflie_6DoF_pose.txt'
 
 textPoseData_optitrack = importdata(textFileDir_optitrack, delimiter, headerlinesIn);
 CFPoseTime_optitrack = textPoseData_optitrack.data(:,1).';
@@ -50,10 +51,11 @@ for k = 1:numPose_optitrack
     stateEsti_CF_optitrack(4:6,k) = [roll_optitrack; pitch_optitrack; yaw_optitrack];
 end
 
-%% Parse Crazyflie point cloud data (1x19): Optitrack 
+%% 2.Parse Crazyflie point cloud data (1x19): Optitrack 
 
 % parsing Crazyflie point cloud data text file
-textFileDir_pointcloud_Optitrack = 'input\global_pointcloud_1x19_optitrack.txt'
+%textFileDir_pointcloud_Optitrack = 'input\global_pointcloud_1x19_optitrack.txt'
+textFileDir_pointcloud_Optitrack = 'input\global_pointcloud_1x19.txt'
 
 textCFPointCloudData_Optitrack = importdata(textFileDir_pointcloud_Optitrack, delimiter, headerlinesIn);
 
@@ -66,20 +68,23 @@ for k = 1:numPointCloud_Optitrack
     pointcloud_CF_Optitrack(:,k)=CFPointCloudData_Optitrack(k,:);
 end
 
-%% Manhattan frame mapping parameters
+%% 3.Manhattan frame mapping parameters
 
 RANSAC_LINE_INLIER_TH = 0.05; % [m], 랜덤으로 생성한 직선과 어떤 점과의 거리가 이 값 안에 있으면 inlier point로 취급
-NUM_INLIER_POINTS_TH = 30;
+NUM_INLIER_POINTS_TH = 40;
 ANGLE_TH = 20; % [deg], MF_X축과의 각도차이가 이 값 이하이면 MF_X축과 평행한 것으로 취급
 TH_DISTANCE_BETWEEN_REFITTED_LINE = 0.3; % [m]
-PARALLEL_OFFSET_TH = 0.1; % [m] walls 에 저장된 평행한 두 직선의 거리 차이가 이 값 이하이면 첫 번째 line으로 합침 
-ceiling_height = 2.5; % [m]
+PARALLEL_OFFSET_TH = 0.7; % [m] walls 에 저장된 평행한 두 직선의 거리 차이가 이 값 이하이면 첫 번째 line으로 합침 
+%ceiling_height = 2.5; % [m]
+pointsIdxInThres_accumulate = [];
 
 used_inlierPts_flag = 0;
 walls_flag = 0;
 
 used_inlierPts_x_accumulate = [];
 idx_unique_used_inlierPts_accumulate = [];
+
+used_points_accumulate = [];
 
 walls = []; % 여기는 이제 전체 walls (MW_Map)
 
@@ -132,11 +137,17 @@ for k = 1: numPose_optitrack
     % line을 만드는 데 사용된 point 제거
     if used_inlierPts_flag == 0
     else
-        for i = 1:size(unique_used_inlierPts_x_accumulate,2)
-            idx_unique_used_inlierPts_accumulate = [idx_unique_used_inlierPts_accumulate find(unique_used_inlierPts_x_accumulate(i)==pointCloud_original(1,:))];
-        end
-        idx_unique_used_inlierPts_accumulate = unique(idx_unique_used_inlierPts_accumulate);
-        pointCloud(:,idx_unique_used_inlierPts_accumulate) = [];
+        % 새로운 pointCloud에서 used_points_accumulate 제거
+        pointCloud_1 = pointCloud(1,:);
+        pointCloud_2 = pointCloud(2,:);
+        pointCloud_3 = pointCloud(3,:);
+            
+        new_pointCloud_1 = setdiff(pointCloud_1,used_points_accumulate(1,:),'stable');
+        new_pointCloud_2 = setdiff(pointCloud_2,used_points_accumulate(2,:),'stable');
+        new_pointCloud_3 = setdiff(pointCloud_3,used_points_accumulate(3,:),'stable');
+        new_pointCloud = [new_pointCloud_1;new_pointCloud_2;new_pointCloud_3];
+
+        pointCloud = new_pointCloud;
     end
     
     %% Line 늘리기
@@ -168,6 +179,7 @@ for k = 1: numPose_optitrack
 
             elseif walls(i).alignment == 'x'
                 pointsIdxInThres = PointsInThres(pointCloud, walls(i).refittedLineModel, TH_DISTANCE_BETWEEN_REFITTED_LINE);
+%                 pointsIdxInThres_accumulate = [pointsIdxInThres_accumulate pointsIdxInThres];
                     
                 if isempty(pointsIdxInThres) ~=0
                     continue;
@@ -186,7 +198,7 @@ for k = 1: numPose_optitrack
 
 
 %                     % 각 line에 새로 포함된 point 개수 추가 
-%                     walls(i).score = length(pointsIdxInThres); % 이건 아님 다시 생각해보기
+%                     walls(i).score = length(pointsIdxInThres_accumulate);
                 end 
             end
         end
@@ -196,129 +208,144 @@ for k = 1: numPose_optitrack
     while(true)
 
         % do line RANSAC  
-        [lineIdx, ~, lineModel] = detectLineRANSAC(pointCloud, RANSAC_LINE_INLIER_TH); % find inlier points and line model (a,b,c)
-         
-
-        % while loop terminating condition
-        if size(lineIdx,2) < NUM_INLIER_POINTS_TH % if the number of inlier points is less than the threshold, then do not build a line model
+        if size(pointCloud,2) <= 1
             break
-        end      
+        elseif size(pointCloud,2) >= 2
+            [lineIdx, ~, lineModel] = detectLineRANSAC(pointCloud, RANSAC_LINE_INLIER_TH); % find inlier points and line model (a,b,c)
+             
+    
+            % while loop terminating condition
+            if size(lineIdx,2) < NUM_INLIER_POINTS_TH % if the number of inlier points is less than the threshold, then do not build a line model
+                break
+            end      
+    
+    
+            % line RANSAC으로 생성한 lineModel의 middle point
+            middle_x = (min(pointCloud(1,lineIdx))+max(pointCloud(1,lineIdx)))/2;
+            middle_y = (-lineModel(1)*middle_x - lineModel(3))/lineModel(2);
+            middlePoint = [middle_x middle_y];
+            
+    
+            % 1) Angle difference between line and Manhattan frame --> refit slope
+            slope_line = -(lineModel(1)/lineModel(2));
+            if angleBetweenTwo3DVectors(MF_X, slope_line) < ANGLE_TH %[deg]
+                refitted_slope_line = MF_X(2)/MF_X(1); % MF X축과 평행관계로 refit
+    
+                % middlePoint를 지나고 기울기가 refitted_slope_line인 직선으로 변경
+                % reffited line: a*x + b*y + c = 0
+                a = refitted_slope_line;
+                b = -1;
+                c = middlePoint(2) - (refitted_slope_line * middlePoint(1));
+                refittedLineModel = [a b c];
+             
+                xmin = min(pointCloud(1,lineIdx));
+                xmax = max(pointCloud(1,lineIdx));
+    
+                x_line = [xmin, xmax];
+                y_line = refitted_slope_line * x_line + (middlePoint(2) - (refitted_slope_line * middlePoint(1)));
+    
+                % walls struct field
+                alignment = 'y'; % Manhattan frame(MF) Y축에 수직
+                offset = c/sqrt((refitted_slope_line^2)+1);
+                n = MF_Y;
+                score = size(lineIdx,2);
+                refittedLineModel = refittedLineModel; % 이건 line 늘리기 때 확인용
+    %             max_xy_M = [xmax a*xmax+c]; % 사실 양 끝점
+    %             min_xy_M = [xmin a*xmin+c]; 
+                max_xy_M = [xmax max(pointCloud(2,lineIdx))];
+                min_xy_M = [xmin min(pointCloud(2,lineIdx))]; 
+               
+                % aumgent walls
+                walls = [walls; struct('alignment', alignment, 'offset', offset, 'n', n, 'score', score, 'refittedLineModel', refittedLineModel, 'max_xy_M', max_xy_M, 'min_xy_M', min_xy_M)];
+                walls_flag = walls_flag + 1;
+    
+                % walls 내의 line들 중에서 제거할 것이 있으면 제거하기
+                removeUnnecessaryLineInWalls
+    
+            %%
+            else
+    
+                refitted_slope_line = -1/(MF_X(2)/MF_X(1)); % MF Y축과 평행관계로 refit
+    
+                % middlePoint를 지나고 기울기가 refitted_slope_line인 직선으로 변경
+                % reffited line: a*x + b*y + c = 0
+                a = refitted_slope_line;
+                b = -1;
+                c = middlePoint(2) - (refitted_slope_line * middlePoint(1));
+                refittedLineModel = [a b c];
+                
+                
+                ymin = min(pointCloud(2,lineIdx));
+                ymax = max(pointCloud(2,lineIdx));
+    
+                y_line = [ymin, ymax];
+                x_line = (y_line - (middlePoint(2) - (refitted_slope_line * middlePoint(1))))/refitted_slope_line;   
+    
+    
+                % walls struct field
+                alignment = 'x'; % Manhattan frame(MF) X축에 수직
+                offset = c/sqrt((refitted_slope_line^2)+1);
+                n = MF_X;
+                score = size(lineIdx,2);
+                refittedLineModel = refittedLineModel; % 이건 line 늘리기 때 확인용
+    %             max_xy_M = [(ymax-c)/a ymax]; % 사실 양 끝점
+    %             min_xy_M = [(ymin-c)/a ymin];
+                max_xy_M = [max(pointCloud(1,lineIdx)) ymax]; 
+                min_xy_M = [min(pointCloud(1,lineIdx)) ymin];
+    
+    
+                % aumgent walls
+                walls = [walls; struct('alignment', alignment, 'offset', offset, 'n', n, 'score', score, 'refittedLineModel', refittedLineModel, 'max_xy_M', max_xy_M, 'min_xy_M', min_xy_M)];   
+                walls_flag = walls_flag + 1;
+    
+                % walls 내의 line들 중에서 제거할 것이 있으면 제거하기
+                removeUnnecessaryLineInWalls
+    
+            end
+    
+            % plot line RANSAC results 
+%             figure; % figure(2) 대신 이걸로 하면 line 한 개씩 각 단계가 따로따로 그려진다. 단계별로 확인하기 좋음
+%             %figure(2);
+%             plot(pointCloud(1,:), pointCloud(2,:), 'k*'); hold on; grid on; axis equal;
+%             plot(pointCloud(1,lineIdx), pointCloud(2,lineIdx), 'ro'); % 원래는 lineIdx
+%             %line(x_line, y_line, 'color', 'm', 'LineWidth', 2);
+%     
+%             % 여긴 이제 고쳐야 됨. 합친 게 반영 안됐음. walls에 누적하는 족족 그리니까. 지금은 제거랑 별개임.********
+%             if angleBetweenTwo3DVectors(MF_X, slope_line) < ANGLE_TH
+%                 line(x_line, y_line, 'color', 'g', 'marker','s','LineWidth', 3);
+%             else
+%                 line(x_line, y_line, 'color', 'r', 'marker','s','LineWidth', 3);
+%             end
+%             xlabel('X[m]','FontSize',15,'fontname','times new roman') ;
+%             ylabel('Y[m]','FontSize',15,'fontname','times new roman');
+%             set(gcf,'Color','w');
+%             set(gca,'FontSize',15,'fontname','times new roman');
+%             hold on;      
+            %%        
+            axis([ -2.4529    5.5749   -1.1148    5.2168])       
 
+            pointCloud(:,lineIdx) = [];
 
-        % line RANSAC으로 생성한 lineModel의 middle point
-        middle_x = (min(pointCloud(1,lineIdx))+max(pointCloud(1,lineIdx)))/2;
-        middle_y = (-lineModel(1)*middle_x - lineModel(3))/lineModel(2);
-        middlePoint = [middle_x middle_y];
-        
+          % pointCloud_original에서 pointCloud를 제거한 것이 used_points 이다. 여기서 used_points를 정의한다.
+            pointCloud_original_1 = pointCloud_original(1,:);
+            pointCloud_original_2 = pointCloud_original(2,:);
+            pointCloud_original_3 = pointCloud_original(3,:);
+            
+            used_points_1 = setdiff(pointCloud_original_1,pointCloud(1,:),'stable');
+            used_points_2 = setdiff(pointCloud_original_2,pointCloud(2,:),'stable');
+            used_points_3 = setdiff(pointCloud_original_3,pointCloud(3,:),'stable');
+            used_points = [used_points_1;used_points_2;used_points_3];
 
-        % 1) Angle difference between line and Manhattan frame --> refit slope
-        slope_line = -(lineModel(1)/lineModel(2));
-        if angleBetweenTwo3DVectors(MF_X, slope_line) < ANGLE_TH %[deg]
-            refitted_slope_line = MF_X(2)/MF_X(1); % MF X축과 평행관계로 refit
+            used_points_accumulate = [used_points_accumulate used_points];
 
-            % middlePoint를 지나고 기울기가 refitted_slope_line인 직선으로 변경
-            % reffited line: a*x + b*y + c = 0
-            a = refitted_slope_line;
-            b = -1;
-            c = middlePoint(2) - (refitted_slope_line * middlePoint(1));
-            refittedLineModel = [a b c];
-         
-            xmin = min(pointCloud(1,lineIdx));
-            xmax = max(pointCloud(1,lineIdx));
-
-            x_line = [xmin, xmax];
-            y_line = refitted_slope_line * x_line + (middlePoint(2) - (refitted_slope_line * middlePoint(1)));
-
-            % walls struct field
-            alignment = 'y'; % Manhattan frame(MF) Y축에 수직
-            offset = c/sqrt((refitted_slope_line^2)+1);
-            n = MF_Y;
-            score = size(lineIdx,2);
-            refittedLineModel = refittedLineModel; % 이건 line 늘리기 때 확인용
-%             max_xy_M = [xmax a*xmax+c]; % 사실 양 끝점
-%             min_xy_M = [xmin a*xmin+c]; 
-            max_xy_M = [xmax max(pointCloud(2,lineIdx))];
-            min_xy_M = [xmin min(pointCloud(2,lineIdx))]; 
+            used_inlierPts_flag = used_inlierPts_flag + 1;
            
-            % aumgent walls
-            walls = [walls; struct('alignment', alignment, 'offset', offset, 'n', n, 'score', score, 'refittedLineModel', refittedLineModel, 'max_xy_M', max_xy_M, 'min_xy_M', min_xy_M)];
-            walls_flag = walls_flag + 1;
-
-            % walls 내의 line들 중에서 제거할 것이 있으면 제거하기
-            removeUnnecessaryLineInWalls
-
-        %%
-        else
-
-            refitted_slope_line = -1/(MF_X(2)/MF_X(1)); % MF Y축과 평행관계로 refit
-
-            % middlePoint를 지나고 기울기가 refitted_slope_line인 직선으로 변경
-            % reffited line: a*x + b*y + c = 0
-            a = refitted_slope_line;
-            b = -1;
-            c = middlePoint(2) - (refitted_slope_line * middlePoint(1));
-            refittedLineModel = [a b c];
-            
-            
-            ymin = min(pointCloud(2,lineIdx));
-            ymax = max(pointCloud(2,lineIdx));
-
-            y_line = [ymin, ymax];
-            x_line = (y_line - (middlePoint(2) - (refitted_slope_line * middlePoint(1))))/refitted_slope_line;   
-
-
-            % walls struct field
-            alignment = 'x'; % Manhattan frame(MF) X축에 수직
-            offset = c/sqrt((refitted_slope_line^2)+1);
-            n = MF_X;
-            score = size(lineIdx,2);
-            refittedLineModel = refittedLineModel; % 이건 line 늘리기 때 확인용
-%             max_xy_M = [(ymax-c)/a ymax]; % 사실 양 끝점
-%             min_xy_M = [(ymin-c)/a ymin];
-            max_xy_M = [max(pointCloud(1,lineIdx)) ymax]; 
-            min_xy_M = [min(pointCloud(1,lineIdx)) ymin];
-
-
-            % aumgent walls
-            walls = [walls; struct('alignment', alignment, 'offset', offset, 'n', n, 'score', score, 'refittedLineModel', refittedLineModel, 'max_xy_M', max_xy_M, 'min_xy_M', min_xy_M)];   
-            walls_flag = walls_flag + 1;
-
-            % walls 내의 line들 중에서 제거할 것이 있으면 제거하기
-            removeUnnecessaryLineInWalls
-
         end
-
-        % plot line RANSAC results 
-        %figure; % figure(2) 대신 이걸로 하면 line 한 개씩 각 단계가 따로따로 그려진다. 단계별로 확인하기 좋음
-%         figure(2);
-%         plot(pointCloud(1,:), pointCloud(2,:), 'k.'); hold on; grid on; axis equal;
-%         plot(pointCloud(1,lineIdx), pointCloud(2,lineIdx), 'r.'); % 원래는 lineIdx
-%         %line(x_line, y_line, 'color', 'm', 'LineWidth', 2);
-% 
-%         % 여긴 이제 고쳐야 됨. 합친 게 반영 안됐음. walls에 누적하는 족족 그리니까. 지금은 제거랑 별개임.********
-%         if angleBetweenTwo3DVectors(MF_X, slope_line) < ANGLE_TH
-%             line(x_line, y_line, 'color', 'g', 'marker','s','LineWidth', 3);
-%         else
-%             line(x_line, y_line, 'color', 'r', 'marker','s','LineWidth', 3);
-%         end
-%         xlabel('X[m]','FontSize',15,'fontname','times new roman') ;
-%         ylabel('Y[m]','FontSize',15,'fontname','times new roman');
-%         set(gcf,'Color','w');
-%         set(gca,'FontSize',15,'fontname','times new roman');
-        hold on;      
-        %%        
-        axis([ -2.4529    5.5749   -1.1148    5.2168])       
-            
-        used_inlierPts_x = [pointCloud(1,lineIdx)]; % 원래는 lineIdx % line 만드는데 사용된 points x좌표 (inlier points)
-        used_inlierPts_x_accumulate = [used_inlierPts_x_accumulate used_inlierPts_x];
-        unique_used_inlierPts_x_accumulate = unique(used_inlierPts_x_accumulate);
-        used_inlierPts_flag = used_inlierPts_flag + 1;
-
-        pointCloud(:,lineIdx) = [];
+        
     end
 
     %plot_2Dmap_line_xyplane
-    plot_plane
+    plot_plane; view(47, 48);
 
 
     % 여기는 while문을 빠져나오고 아무것도 안 할 때 (RANSAC으로 생성한 line의 inlier point 개수가 TH 미만이어서 walls에 추가도 안하고 plot도 안 함
